f4a1fe9ae7e9e214284ad48d4151e5004db1c9c2
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-04-18 10:56:55 +0200

    Add the correction of this dojo

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,26 @@ class HomeController extends AbstractController
      */
     public function index(): string
     {
+        $errors = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $data = array_map('trim', $_POST);
+
+            if (!isset($data['search']) || empty($data['search'])) {
+                $errors[] = "Il n'y a pas de recherche";
+            }
+
+            if (empty($errors)) {
+                $search = $data['search'];
+                $page = $this->twig->render('Home/article.html.twig');
+
+
+                $page = str_replace($search, '<span style="background-color: yellow;">' . $search . '</span>', $page);
+
+                return $page;
+            }
+        }
+
         return $this->twig->render('Home/article.html.twig');
     }
 }
